312a7569dadebbe6be540ab7c28b90f2821b6902
Mikhail Shemchuk <mihey.by@gmail.com>
2020-10-27 22:11:52 +0300

    feat: solve count cats and carbon dating tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,20 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  
+  if (!sampleActivity || 
+    typeof sampleActivity !== 'string') {
+      return false;
+    }
+    
+  let activity = +sampleActivity;
+
+  if (typeof activity === 'number' && 
+      activity > 0 &&
+      activity < MODERN_ACTIVITY ) {
+    return Math.ceil(HALF_LIFE_PERIOD * MODERN_ACTIVITY / sampleActivity);
+  } else {
+    return false;
+  }
 };
