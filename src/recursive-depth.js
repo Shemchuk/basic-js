4a290de3ee4c00788be269dee5a83b4ccea7c19c
Mikhail Shemchuk <mihey.by@gmail.com>
2020-10-28 17:16:40 +0300

    feat: solve recursive-depth and simple-chain

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 1;
+    if ( JSON.stringify(arr) === JSON.stringify(arr.flat()) ) {
+      return count;
+    } else {
+      count += this.calculateDepth(arr.flat());
+      return count;
+    }
   }
 };
